1b5ad81aa1ef640dd05847f15f38ca4617c80de7
Alijah LeMay <36865734+Alijah-LeMay@users.noreply.github.com>
2026-04-24 22:26:04 -0700

    Create map.js

---
+++ map.js
@@ -0,0 +1,7 @@
+// Simple example of a map function
+// Key note is that map returns a new array, it does NOT modify an existing one
+let myArr=[1,2,3,4]
+
+let doubled = myArr.map(x=> x*2)
+
+console.log(doubled)
